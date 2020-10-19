d0f48490200219b88bfacab31df24f24a73bfad6
axelpotier-astro <axel.potier@obspm.fr>
2020-10-19 13:39:04 -0700

    Modify initial parameters

---
+++ Run_Simulation.py
@@ -0,0 +1,7 @@
+import os
+from Main_EFC_THDv2 import *
+import Main_EFC_THDv2 as main
+
+#dict={'Name_Experiment': 'Experiment1'}
+main.create_interaction_matrices(os.getcwd()+'/Essai_param.ini')
+main.phase,im=CorrectionLoop(os.getcwd()+'/Essai_param.ini')#,NewSIMUconfig=dict)
