0a0106534a923b95820d0ed052e167d88a1241e1
Johan Mazoyer <johan.mazoyer@gmail.com>
2020-10-25 12:39:25 +0100

    added a parameter file to avoid syncing the users parameter files

--- Run_Simulation.py
+++ Run_Simulation.py
@@ -3,5 +3,5 @@ from Main_EFC_THD import *
 import Main_EFC_THD as main
 
 #dict={'Name_Experiment': 'Experiment1'}
-main.create_interaction_matrices(os.getcwd()+'/Essai_param.ini')
-main.phase,im=CorrectionLoop(os.getcwd()+'/Essai_param.ini')#,NewSIMUconfig=dict)
+main.create_interaction_matrices(os.getcwd()+'/parameter_files/First_test_params.ini')
+main.phase,im=CorrectionLoop(os.getcwd()+'/parameter_files/First_test_params.ini')#,NewSIMUconfig=dict)
