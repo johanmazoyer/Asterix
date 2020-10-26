dd18927cc3c067b6cf12ef67d755034304df9983
Johan Mazoyer <johan.mazoyer@gmail.com>
2020-10-26 18:05:29 +0100

    clean fit functions and WSC functiosn

--- Asterix/WSC_functions.py
+++ Asterix/WSC_functions.py
@@ -1,14 +1,23 @@
-
+import numpy as np
 import matplotlib.pyplot as plt
 
-from Asterix.shortcuts import *
+# from Asterix.shortcuts import *
 import Asterix.InstrumentSimu_functions as instr
 
 import Asterix.processing_functions as proc
 
 
-def invertSVD(matrix_to_invert,cut,goal='e',regul='truncation',visu=True,otherbasis=False,basisDM3=0,intermatrix_dir=''):
-    ''' --------------------------------------------------
+def invertSVD(
+    matrix_to_invert,
+    cut,
+    goal="e",
+    regul="truncation",
+    visu=True,
+    otherbasis=False,
+    basisDM3=0,
+    intermatrix_dir="",
+):
+    """ --------------------------------------------------
     Invert a matrix after a Singular Value Decomposition. The inversion can be regularized.
     
     Parameters:
@@ -30,43 +39,51 @@ def invertSVD(matrix_to_invert,cut,goal='e',regul='truncation',visu=True,otherba
     np.diag(InvS): Inverse eigenvalues of the input matrix
     np.diag(InvS_truncated): Inverse eigenvalues of the input matrix after regularization
     pseudoinverse: Regularized inverse of the input matrix
-    -------------------------------------------------- '''
+    -------------------------------------------------- """
     U, s, V = np.linalg.svd(matrix_to_invert, full_matrices=False)
-    #print(s)
+    # print(s)
     S = np.diag(s)
-    InvS=np.linalg.inv(S)
-    InvS_truncated=np.linalg.inv(S)
-    #print(InvS)
-    if(visu==True):
-        plt.plot(np.diag(InvS),'r.')
-        plt.yscale('log')
-    #     plt.savefig(intermatrix_dir+'invertSVDEFC_'+ '_'.join(map(str, choosepix)) + 'pix_' + str(amplitudeEFC) + 'nm_.png')  
-    
-    if(goal=='e'):
-        InvS_truncated[np.where(InvS_truncated>cut)]=0
-        pseudoinverse=np.dot(np.dot(np.transpose(V),InvS_truncated),np.transpose(U))
-      
-    if(goal=='c'):
-        if regul=='truncation':
-            InvS_truncated[cut:]=0
-        if regul=='tikhonov':
-            InvS_truncated=np.diag(s/(s**2+s[cut]**2))
-            if(visu==True):
-                plt.plot(np.diag(InvS_truncated),'b.')
-                plt.yscale('log')
-                #plt.show()
-        pseudoinverse=np.dot(np.dot(np.transpose(V),InvS_truncated),np.transpose(U))
-    
-    if (otherbasis==True):
-        pseudoinverse=np.dot(np.transpose(basisDM3),pseudoinverse)
-        
-    return [np.diag(InvS),np.diag(InvS_truncated),pseudoinverse]
+    InvS = np.linalg.inv(S)
+    InvS_truncated = np.linalg.inv(S)
+    # print(InvS)
+    if visu == True:
+        plt.plot(np.diag(InvS), "r.")
+        plt.yscale("log")
+    #     plt.savefig(intermatrix_dir+'invertSVDEFC_'+ '_'.join(map(str, choosepix)) + 'pix_' + str(amplitudeEFC) + 'nm_.png')
 
-    
-    
+    if goal == "e":
+        InvS_truncated[np.where(InvS_truncated > cut)] = 0
+        pseudoinverse = np.dot(np.dot(np.transpose(V), InvS_truncated), np.transpose(U))
 
-def createvectorprobes(wavelength,entrancepupil,coro_mask,lyot_mask,amplitude,posprobes,pushact,dimimages,cutsvd):
-    ''' --------------------------------------------------
+    if goal == "c":
+        if regul == "truncation":
+            InvS_truncated[cut:] = 0
+        if regul == "tikhonov":
+            InvS_truncated = np.diag(s / (s ** 2 + s[cut] ** 2))
+            if visu == True:
+                plt.plot(np.diag(InvS_truncated), "b.")
+                plt.yscale("log")
+                # plt.show()
+        pseudoinverse = np.dot(np.dot(np.transpose(V), InvS_truncated), np.transpose(U))
+
+    if otherbasis == True:
+        pseudoinverse = np.dot(np.transpose(basisDM3), pseudoinverse)
+
+    return [np.diag(InvS), np.diag(InvS_truncated), pseudoinverse]
+
+
+def createvectorprobes(
+    wavelength,
+    entrancepupil,
+    coro_mask,
+    lyot_mask,
+    amplitude,
+    posprobes,
+    pushact,
+    dimimages,
+    cutsvd,
+):
+    """ --------------------------------------------------
     Build the interaction matrix for pair-wise probing.
     
     Parameters:
@@ -86,45 +103,53 @@ def createvectorprobes(wavelength,entrancepupil,coro_mask,lyot_mask,amplitude,po
     ------
     PWVector: 2D array, vector probe to be multiplied by the image difference matrix in order to retrieve the focal plane electric field
     SVD: 2D array, map of the inverse singular values for each pixels and before regularization
-    -------------------------------------------------- '''
-    isz=len(pushact[0])
-    numprobe=len(posprobes)
-    deltapsik=np.zeros((numprobe,dimimages,dimimages),dtype=complex)
-    probephase=np.zeros((numprobe,isz,isz))
-    matrix=np.zeros((numprobe,2))
-    PWVector=np.zeros((dimimages**2,2,numprobe))
-    SVD=np.zeros((2,dimimages,dimimages))
-    squaremaxPSF=np.amax(np.abs(instr.pupiltodetector(entrancepupil,1,lyot_mask)))
-    k=0
+    -------------------------------------------------- """
+    isz = len(pushact[0])
+    numprobe = len(posprobes)
+    deltapsik = np.zeros((numprobe, dimimages, dimimages), dtype=complex)
+    probephase = np.zeros((numprobe, isz, isz))
+    matrix = np.zeros((numprobe, 2))
+    PWVector = np.zeros((dimimages ** 2, 2, numprobe))
+    SVD = np.zeros((2, dimimages, dimimages))
+    squaremaxPSF = np.amax(np.abs(instr.pupiltodetector(entrancepupil, 1, lyot_mask)))
+    k = 0
     for i in posprobes:
-        probephase[k]=amplitude*pushact[i]
-        probephase[k]=2*np.pi*(probephase[k])*1e-9/wavelength
-        inputwavefront=entrancepupil*(1+1j*probephase[k])
-        deltapsikbis=instr.pupiltodetector(inputwavefront,coro_mask,lyot_mask,perfect_coro=True,perfect_entrance_pupil=entrancepupil)/squaremaxPSF
-        deltapsik[k]=proc.resampling(deltapsikbis,dimimages)
-        k=k+1
+        probephase[k] = amplitude * pushact[i]
+        probephase[k] = 2 * np.pi * (probephase[k]) * 1e-9 / wavelength
+        inputwavefront = entrancepupil * (1 + 1j * probephase[k])
+        deltapsikbis = (
+            instr.pupiltodetector(
+                inputwavefront,
+                coro_mask,
+                lyot_mask,
+                perfect_coro=True,
+                perfect_entrance_pupil=entrancepupil,
+            )
+            / squaremaxPSF
+        )
+        deltapsik[k] = proc.resampling(deltapsikbis, dimimages)
+        k = k + 1
 
-    l=0
+    l = 0
     for i in np.arange(dimimages):
         for j in np.arange(dimimages):
-            matrix[:,0]=real(deltapsik[:,i,j])
-            matrix[:,1]=im(deltapsik[:,i,j])
-    
+            matrix[:, 0] = np.real(deltapsik[:, i, j])
+            matrix[:, 1] = np.imag(deltapsik[:, i, j])
+
             try:
-                inversion=invertSVD(matrix,cutsvd,visu=False)
-                SVD[:,i,j]=inversion[0]
-                PWVector[l]=inversion[2]
+                inversion = invertSVD(matrix, cutsvd, visu=False)
+                SVD[:, i, j] = inversion[0]
+                PWVector[l] = inversion[2]
             except:
-                print('Careful: Error! for l='+str(l))
-                SVD[:,i,j]=np.zeros(2)
-                PWVector[l]=np.zeros((2,numprobe))
-            l=l+1  
-    return [PWVector,SVD]
-   
-    
-    
-def creatingWhichinPupil(pushact,entrancepupil,cutinpupil):
-    ''' --------------------------------------------------
+                print("Careful: Error! for l=" + str(l))
+                SVD[:, i, j] = np.zeros(2)
+                PWVector[l] = np.zeros((2, numprobe))
+            l = l + 1
+    return [PWVector, SVD]
+
+
+def creatingWhichinPupil(pushact, entrancepupil, cutinpupil):
+    """ --------------------------------------------------
     Create a vector with the index of all the actuators located in the entrance pupil
     
     Parameters:
@@ -136,21 +161,22 @@ def creatingWhichinPupil(pushact,entrancepupil,cutinpupil):
     Return:
     ------
     WhichInPupil: 1D array, index of all the actuators located inside the pupil
-    -------------------------------------------------- '''
+    -------------------------------------------------- """
     WhichInPupil = []
     for i in np.arange(int(1024)):
-        Psivector=pushact[i]
-        cut=cutinpupil*np.sum(Psivector)
-        if(np.sum(Psivector*entrancepupil)>cut):
+        Psivector = pushact[i]
+        cut = cutinpupil * np.sum(Psivector)
+        if np.sum(Psivector * entrancepupil) > cut:
             WhichInPupil.append(i)
-    
+
     WhichInPupil = np.array(WhichInPupil)
     return WhichInPupil
-    
 
 
-def creatingMaskDH(dimimages,shape,choosepixDH=[0,0,0,0], inner=0, outer=0, xdecay=0):
-    ''' --------------------------------------------------
+def creatingMaskDH(
+    dimimages, shape, choosepixDH=[0, 0, 0, 0], inner=0, outer=0, xdecay=0
+):
+    """ --------------------------------------------------
     Create a binary mask.
     
     Parameters:
@@ -165,26 +191,39 @@ def creatingMaskDH(dimimages,shape,choosepixDH=[0,0,0,0], inner=0, outer=0, xdec
     Return:
     ------
     maskDH: 2D array, binary mask
-    -------------------------------------------------- '''
-    xx, yy = np.meshgrid(np.arange(dimimages)-(dimimages)/2, np.arange(dimimages)-(dimimages)/2)
-    rr     = np.hypot(yy, xx)
-    if shape=='square':
-        maskDH=np.ones((dimimages,dimimages))
-        maskDH[xx<choosepixDH[0]]=0
-        maskDH[xx>choosepixDH[1]]=0
-        maskDH[yy<choosepixDH[2]]=0
-        maskDH[yy>choosepixDH[3]]=0
-    if shape=='circle':
-        maskDH=np.ones((dimimages,dimimages))
-        maskDH[rr>=outer]=0
-        maskDH[rr<inner]=0
-        maskDH[xx<xdecay]=0
+    -------------------------------------------------- """
+    xx, yy = np.meshgrid(
+        np.arange(dimimages) - (dimimages) / 2, np.arange(dimimages) - (dimimages) / 2
+    )
+    rr = np.hypot(yy, xx)
+    if shape == "square":
+        maskDH = np.ones((dimimages, dimimages))
+        maskDH[xx < choosepixDH[0]] = 0
+        maskDH[xx > choosepixDH[1]] = 0
+        maskDH[yy < choosepixDH[2]] = 0
+        maskDH[yy > choosepixDH[3]] = 0
+    if shape == "circle":
+        maskDH = np.ones((dimimages, dimimages))
+        maskDH[rr >= outer] = 0
+        maskDH[rr < inner] = 0
+        maskDH[xx < xdecay] = 0
     return maskDH
 
 
-
-def creatingCorrectionmatrix(entrancepupil,coro_mask,lyot_mask,dimimages,wavelength,amplitude,pushact,mask,Whichact,otherbasis=False,basisDM3=0):
-    ''' --------------------------------------------------
+def creatingCorrectionmatrix(
+    entrancepupil,
+    coro_mask,
+    lyot_mask,
+    dimimages,
+    wavelength,
+    amplitude,
+    pushact,
+    mask,
+    Whichact,
+    otherbasis=False,
+    basisDM3=0,
+):
+    """ --------------------------------------------------
     Create the jacobian matrix for Electric Field Conjugation
     
     Parameters:
@@ -204,37 +243,51 @@ def creatingCorrectionmatrix(entrancepupil,coro_mask,lyot_mask,dimimages,wavelen
     Return:
     ------
     Gmatrixbis: 2D array, jacobian matrix for Electric Field Conjugation
-    -------------------------------------------------- '''
-    #change basis if needed
-    if (otherbasis == True):
-        nb_fct=basisDM3.shape[0]#number of functions in the basis
-        tmp=pushact.reshape(pushact.shape[0],pushact.shape[1]*pushact.shape[2])
-        bas_fct=np.dot(basisDM3,tmp).reshape(nb_fct,pushact.shape[1],pushact.shape[2])
+    -------------------------------------------------- """
+    # change basis if needed
+    if otherbasis == True:
+        nb_fct = basisDM3.shape[0]  # number of functions in the basis
+        tmp = pushact.reshape(pushact.shape[0], pushact.shape[1] * pushact.shape[2])
+        bas_fct = np.dot(basisDM3, tmp).reshape(
+            nb_fct, pushact.shape[1], pushact.shape[2]
+        )
     else:
         bas_fct = np.array([pushact[ind] for ind in Whichact])
-        nb_fct=len(Whichact)
-    squaremaxPSF=np.amax(np.abs(instr.pupiltodetector(entrancepupil,1,lyot_mask)))
-    print('Start EFC')
-    Gmatrixbis=np.zeros((2*int(np.sum(mask)),nb_fct))
-    k=0
+        nb_fct = len(Whichact)
+    squaremaxPSF = np.amax(np.abs(instr.pupiltodetector(entrancepupil, 1, lyot_mask)))
+    print("Start EFC")
+    Gmatrixbis = np.zeros((2 * int(np.sum(mask)), nb_fct))
+    k = 0
     for i in range(nb_fct):
-        if i%100 == 0:
+        if i % 100 == 0:
             print(i)
-        Psivector=amplitude*bas_fct[i]
-        Psivector=2*np.pi*(Psivector)*1e-9/wavelength
-        inputwavefront=entrancepupil*(1+1j*Psivector)
-        Gvector=instr.pupiltodetector(inputwavefront,coro_mask,lyot_mask,perfect_coro=True,perfect_entrance_pupil=entrancepupil)/squaremaxPSF
-        Gvector=proc.resampling(Gvector,dimimages)
-        Gmatrixbis[0:int(np.sum(mask)),k]=real(Gvector[np.where(mask==1)]).flatten()
-        Gmatrixbis[int(np.sum(mask)):,k]=im(Gvector[np.where(mask==1)]).flatten()
-        k=k+1
-    print('End EFC')
+        Psivector = amplitude * bas_fct[i]
+        Psivector = 2 * np.pi * (Psivector) * 1e-9 / wavelength
+        inputwavefront = entrancepupil * (1 + 1j * Psivector)
+        Gvector = (
+            instr.pupiltodetector(
+                inputwavefront,
+                coro_mask,
+                lyot_mask,
+                perfect_coro=True,
+                perfect_entrance_pupil=entrancepupil,
+            )
+            / squaremaxPSF
+        )
+        Gvector = proc.resampling(Gvector, dimimages)
+        Gmatrixbis[0 : int(np.sum(mask)), k] = np.real(
+            Gvector[np.where(mask == 1)]
+        ).flatten()
+        Gmatrixbis[int(np.sum(mask)) :, k] = np.imag(
+            Gvector[np.where(mask == 1)]
+        ).flatten()
+        k = k + 1
+    print("End EFC")
     return Gmatrixbis
-    
 
 
-def solutionEFC(mask,Result_Estimate,inversed_jacobian,WhichInPupil):
-    ''' --------------------------------------------------
+def solutionEFC(mask, Result_Estimate, inversed_jacobian, WhichInPupil):
+    """ --------------------------------------------------
     Voltage to apply on the deformable mirror in order to minimize the speckle intensity in the dark hole region
     
     Parameters:
@@ -247,23 +300,20 @@ def solutionEFC(mask,Result_Estimate,inversed_jacobian,WhichInPupil):
     Return:
     ------
     solution: 1D array, voltage to apply on each deformable mirror actuator
-    -------------------------------------------------- '''
-    Eab=np.zeros(2*int(np.sum(mask)))
-    Resultatbis=(Result_Estimate[np.where(mask==1)])
-    Eab[0:int(np.sum(mask))]=real(Resultatbis).flatten()     
-    Eab[int(np.sum(mask)):]=im(Resultatbis).flatten()
-    cool=np.dot(inversed_jacobian,Eab)
-        
-    
-    solution=np.zeros(1024)
-    solution[WhichInPupil]=cool
+    -------------------------------------------------- """
+    Eab = np.zeros(2 * int(np.sum(mask)))
+    Resultatbis = Result_Estimate[np.where(mask == 1)]
+    Eab[0 : int(np.sum(mask))] = np.real(Resultatbis).flatten()
+    Eab[int(np.sum(mask)) :] = np.imag(Resultatbis).flatten()
+    cool = np.dot(inversed_jacobian, Eab)
+
+    solution = np.zeros(1024)
+    solution[WhichInPupil] = cool
     return solution
-    
-    
-    
-    
-def solutionEM(mask,Result_Estimate,Hessian_Matrix,Jacobian,WhichInPupil):
-    ''' --------------------------------------------------
+
+
+def solutionEM(mask, Result_Estimate, Hessian_Matrix, Jacobian, WhichInPupil):
+    """ --------------------------------------------------
     Voltage to apply on the deformable mirror in order to minimize the speckle intensity in the dark hole region
     
     Parameters:
@@ -277,24 +327,20 @@ def solutionEM(mask,Result_Estimate,Hessian_Matrix,Jacobian,WhichInPupil):
     Return:
     ------
     solution: 1D array, voltage to apply on each deformable mirror actuator
-    -------------------------------------------------- '''
-    
-    
-    Eab=np.zeros(int(np.sum(mask)))
-    Resultatbis=(Result_Estimate[np.where(mask==1)])
-    Eab=np.real(np.dot(np.transpose(np.conjugate(Jacobian)),Resultatbis)).flatten()
-    cool=np.dot(Hessian_Matrix,Eab)
-        
-    
-    solution=np.zeros(1024)
-    solution[WhichInPupil]=cool
+    -------------------------------------------------- """
+
+    Eab = np.zeros(int(np.sum(mask)))
+    Resultatbis = Result_Estimate[np.where(mask == 1)]
+    Eab = np.real(np.dot(np.transpose(np.conjugate(Jacobian)), Resultatbis)).flatten()
+    cool = np.dot(Hessian_Matrix, Eab)
+
+    solution = np.zeros(1024)
+    solution[WhichInPupil] = cool
     return solution
-    
-    
-    
-    
-def solutionSteepest(mask,Result_Estimate,Hessian_Matrix,Jacobian,WhichInPupil):
-    ''' --------------------------------------------------
+
+
+def solutionSteepest(mask, Result_Estimate, Hessian_Matrix, Jacobian, WhichInPupil):
+    """ --------------------------------------------------
     Voltage to apply on the deformable mirror in order to minimize the speckle intensity in the dark hole region
     
     Parameters:
@@ -308,26 +354,22 @@ def solutionSteepest(mask,Result_Estimate,Hessian_Matrix,Jacobian,WhichInPupil):
     Return:
     ------
     solution: 1D array, voltage to apply on each deformable mirror actuator
-    -------------------------------------------------- '''
-    
-    Eab=np.zeros(int(np.sum(mask)))
-    Resultatbis=(Result_Estimate[np.where(mask==1)])
-    Eab=np.real(np.dot(np.transpose(np.conjugate(Jacobian)),Resultatbis)).flatten()
-    pas=2e3
-    #cool=2*(np.dot(M0,sol)+np.real(np.dot(np.transpose(np.conjugate(G)),Resultatbis))).flatten()
-    cool=pas*2*Eab
-        
-    
-    solution=np.zeros(1024)
-    solution[WhichInPupil]=cool
+    -------------------------------------------------- """
+
+    Eab = np.zeros(int(np.sum(mask)))
+    Resultatbis = Result_Estimate[np.where(mask == 1)]
+    Eab = np.real(np.dot(np.transpose(np.conjugate(Jacobian)), Resultatbis)).flatten()
+    pas = 2e3
+    # cool=2*(np.dot(M0,sol)+np.real(np.dot(np.transpose(np.conjugate(G)),Resultatbis))).flatten()
+    cool = pas * 2 * Eab
+
+    solution = np.zeros(1024)
+    solution[WhichInPupil] = cool
     return solution
 
 
-    
-    
-    
-def FP_PWestimate(Difference,Vectorprobes):
-    ''' --------------------------------------------------
+def FP_PWestimate(Difference, Vectorprobes):
+    """ --------------------------------------------------
     Calculate the focal plane electric field from the prone image differences and the modeled probe matrix
     
     Parameters:
@@ -338,17 +380,18 @@ def FP_PWestimate(Difference,Vectorprobes):
     Return:
     ------
     Difference: 3D array, cube with image difference for each probes. Use for pair-wise probing
-    -------------------------------------------------- '''
-    dimimages=len(Difference[0])
-    numprobe=len(Vectorprobes[0,0])
-    Differenceij=np.zeros((numprobe))
-    Resultat=np.zeros((dimimages,dimimages),dtype=complex)
-    l=0
+    -------------------------------------------------- """
+    dimimages = len(Difference[0])
+    numprobe = len(Vectorprobes[0, 0])
+    Differenceij = np.zeros((numprobe))
+    Resultat = np.zeros((dimimages, dimimages), dtype=complex)
+    l = 0
     for i in np.arange(dimimages):
         for j in np.arange(dimimages):
-            Differenceij[:]=Difference[:,i,j]
-            Resultatbis=np.dot(Vectorprobes[l],Differenceij)
-            Resultat[i,j]=Resultatbis[0]+1j*Resultatbis[1]
-            
-            l=l+1  
-    return Resultat/4
+            Differenceij[:] = Difference[:, i, j]
+            Resultatbis = np.dot(Vectorprobes[l], Differenceij)
+            Resultat[i, j] = Resultatbis[0] + 1j * Resultatbis[1]
+
+            l = l + 1
+    return Resultat / 4
+
--- Asterix/fits_functions.py
+++ Asterix/fits_functions.py
@@ -1,65 +1,10 @@
-#Version 29 Janvier 2020
+import glob
 import numpy as np
 from astropy.io import fits
-import glob
-import os
 
-# def LoadImageFits(docs_dir):
-#     ''' --------------------------------------------------
-#     Load fits file
-#     TO BE REPLACED BY astropy.io.fits FUNCTION DOING JUST THAT : getdata()
-#     Parameters:
-#     ----------
-#     docs_dir: Name of the file with extension
-    
-#     Return:
-#     ------
-#     image: numpy array
-#     -------------------------------------------------- '''
-#     openfits=fits.open(docs_dir)
-#     image=openfits[0].data
-#     return image
-    
-    
-# def SaveFits(image,head,doc_dir2,name,replace=False):
-#     ''' --------------------------------------------------
-#     Save numpy array into a fits file
-#     TO BE REPLACED BY astropy.io.fits FUNCTION DOING JUST THAT : writeto()
 
-#     Parameters:
-#     ----------
-#     image: Array to be saved
-#     head: Header to be saved in this format: ['name',param]
-#     doc_dir2: Directory where the file will be saved
-#     name: Name of the saved file, without extension .fits
-#     replace: Boolean, If True, overwrite on the pre-existing file
-    
-#     Return:
-#     ------
-#     image: numpy array
-#     -------------------------------------------------- '''
-#     hdu=fits.PrimaryHDU(image)
-#     hdul = fits.HDUList([hdu])
-#     hdr=hdul[0].header
-#     head=np.array(head)
-    
-#     if head.ndim>=2:
-        
-#         for newline in np.arange(len(head)):
-#             try:
-#                 hdr.set(head[newline,0][:8],str(head[newline,1]))
-#             except ValueError:
-#                 print('Value error when Fits Saving:' , head[newline,0])
-#                 continue
-
-#     else:
-#         hdr.set(head[0][:8],str(head[1]))
-            
-#     hdu.writeto(doc_dir2+name+'.fits', overwrite=replace )
-    
-    
 def CubeFits(docs_dir):
-    ''' --------------------------------------------------
+    """ --------------------------------------------------
     Load all the fits image in a directory
     
     Parameters:
@@ -69,18 +14,18 @@ def CubeFits(docs_dir):
     Return:
     ------
     image_array: numpy array
-    -------------------------------------------------- '''
+    -------------------------------------------------- """
     image_list = []
-    for filename in sorted(glob.glob(docs_dir+'*.fits')):
-        image=fits.getdata(filename)
+    for filename in sorted(glob.glob(docs_dir + "*.fits")):
+        image = fits.getdata(filename)
         image_list.append(image)
-        
+
     image_array = np.array(image_list)
     return image_array
-    
+
 
 def AverageFits(docs_dir):
-    ''' --------------------------------------------------
+    """ --------------------------------------------------
     Load all the fits in a directory to create an averaged numpy array
     
     Parameters:
@@ -90,18 +35,17 @@ def AverageFits(docs_dir):
     Return:
     ------
     imagemoyenne: numpy array
-    -------------------------------------------------- '''
-    Cubeimage=CubeFits(docs_dir)
-    Sommeimage=0
+    -------------------------------------------------- """
+    Cubeimage = CubeFits(docs_dir)
+    Sommeimage = 0
     for i in np.arange((Cubeimage.shape[0])):
-        Sommeimage=Sommeimage+Cubeimage[i]
-    imagemoyenne=Sommeimage/Cubeimage.shape[0]
+        Sommeimage = Sommeimage + Cubeimage[i]
+    imagemoyenne = Sommeimage / Cubeimage.shape[0]
     return imagemoyenne
-    
-    
-    
+
+
 def GetFluxmetreValue(fitspath):
-    ''' --------------------------------------------------
+    """ --------------------------------------------------
     Extract the information about the fluxmeter in an image header
     
     Parameters:
@@ -111,18 +55,16 @@ def GetFluxmetreValue(fitspath):
     Return:
     ------
     fluxmetre: float
-    -------------------------------------------------- '''
+    -------------------------------------------------- """
     # openfits=fits.open(fitspath)
     # hdu=openfits[0].header
     # fluxmetre=hdu['FLUX_W']
     # return fluxmetre
-    return fits.getval(fitspath,'FLUX_W')
-
-
+    return fits.getval(fitspath, "FLUX_W")
 
 
 def from_param_to_header(config):
-    ''' --------------------------------------------------
+    """ --------------------------------------------------
     Convert ConfigObj parameters to fits header type list
     
     Parameters:
@@ -132,10 +74,10 @@ def from_param_to_header(config):
     Return:
     ------
     header: list of parameters
-    -------------------------------------------------- '''
-    header= fits.Header()
+    -------------------------------------------------- """
+    header = fits.Header()
     for sect in config.sections:
-        #print(config[str(sect)])
+        # print(config[str(sect)])
         for scalar in config[str(sect)].scalars:
             header[str(scalar)[:8]] = str(config[str(sect)][str(scalar)])
     return header
