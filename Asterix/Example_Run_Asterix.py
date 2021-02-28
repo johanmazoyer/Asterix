5db84f6f8c42a6626f9eb54a4c1dfa286ea69592
JOHAN MAZOYER <johan.mazoyer@gmail.com>
2021-02-28 23:02:41 +0100

    time

--- Asterix/Example_Run_Asterix.py
+++ Asterix/Example_Run_Asterix.py
@@ -1,6 +1,10 @@
 import os
 from Asterix import Main_EFC_THD
+import time
 
+start_time = time.time()
 Asterixroot = os.path.dirname(os.path.realpath(__file__))
 Main_EFC_THD.create_interaction_matrices(Asterixroot + os.path.sep+ 'Example_param_file.ini')
+print('time for initialization', time.time() - start_time)
 phase,im=Main_EFC_THD.correctionLoop(Asterixroot + os.path.sep+ 'Example_param_file.ini')#,NewSIMUconfig=dict
+print('total time', time.time() - start_time)
