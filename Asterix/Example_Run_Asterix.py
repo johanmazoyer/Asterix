0a177f06ea875d7508a70124deaf784cec7e64e8
Johan Mazoyer <johan.mazoyer@obspm.fr>
2021-03-26 10:48:26 +0100

    made Example_run a test run to launch before every pull request

--- Asterix/Example_Run_Asterix.py
+++ Asterix/Example_Run_Asterix.py
@@ -12,12 +12,11 @@ Main_EFC_THD.create_interaction_matrices(
     Asterixroot + os.path.sep + 'Example_param_file.ini',
     NewDMconfig={'DM1_active': False},
     NewEstimationconfig={'estimation': 'pw'},
-    NewCorrectionconfig={
-        'circ_side': "right",
-        'Nbiter_corr': "5,5,5",
-        "Nbmode_corr": "350, 380, 400"
-    })
+    NewCorrectionconfig={'circ_side': "right"})
 print('time for 1DM initialization', time.time() - start_time)
+print("")
+print("")
+print("")
 
 # CORRECTION 1DM IN PW + EFC
 start_time = time.time()
@@ -25,12 +24,15 @@ phase, im = Main_EFC_THD.correctionLoop(
     Asterixroot + os.path.sep + 'Example_param_file.ini',
     NewDMconfig={'DM1_active': False},
     NewEstimationconfig={'estimation': 'pw'},
-    NewCorrectionconfig={
-        'circ_side': "right",
-        'Nbiter_corr': "5,5,5",
-        "Nbmode_corr": "350, 380, 400"
+    NewCorrectionconfig={'circ_side': "right"},
+    NewSIMUconfig={
+        'Nbiter_corr': ["5", "5", "5"],
+        "Nbmode_corr": ["350", "380", "400"]
     })
 print('time correction 1DM pw', time.time() - start_time)
+print("")
+print("")
+print("")
 
 # CORRECTION 1DM IN Perfect + EFC
 start_time = time.time()
@@ -38,12 +40,15 @@ phase, im = Main_EFC_THD.correctionLoop(
     Asterixroot + os.path.sep + 'Example_param_file.ini',
     NewDMconfig={'DM1_active': False},
     NewEstimationconfig={'estimation': 'Perfect'},
-    NewCorrectionconfig={
-        'circ_side': "right",
-        'Nbiter_corr': "5,5,5",
-        "Nbmode_corr": "350, 380, 400"
+    NewCorrectionconfig={'circ_side': "right"},
+    NewSIMUconfig={
+        'Nbiter_corr': ["5", "5", "5"],
+        "Nbmode_corr": ["350", "380", "400"]
     })
 print('total time 1DM perfect', time.time() - start_time)
+print("")
+print("")
+print("")
 
 # INITIALISATION 2DM IN PW + EFC
 start_time = time.time()
@@ -51,12 +56,11 @@ Main_EFC_THD.create_interaction_matrices(
     Asterixroot + os.path.sep + 'Example_param_file.ini',
     NewDMconfig={'DM1_active': True},
     NewEstimationconfig={'estimation': 'pw'},
-    NewCorrectionconfig={
-        'circ_side': "Full",
-        'Nbiter_corr': '2  ,1  ,1  ,1   ,3  ,2  ,1  ,2  ,4  ,3',
-        'Nbmode_corr': '500,800,500,1000,700,900,1000,900,700,900'
-    })
+    NewCorrectionconfig={'circ_side': "Full"})
 print('time for 2DM initialization', time.time() - start_time)
+print("")
+print("")
+print("")
 
 # CORRECTION 2DM IN PW + EFC
 start_time = time.time()
@@ -64,12 +68,15 @@ phase, im = Main_EFC_THD.correctionLoop(
     Asterixroot + os.path.sep + 'Example_param_file.ini',
     NewDMconfig={'DM1_active': True},
     NewEstimationconfig={'estimation': 'pw'},
-    NewCorrectionconfig={
-        'circ_side': "Full",
-        'Nbiter_corr': '2  ,1  ,1  ,1   ,3  ,2  ,1  ,2  ,4  ,3',
-        'Nbmode_corr': '500,800,500,1000,700,900,1000,900,700,900'
+    NewCorrectionconfig={'circ_side': "Full"},
+    NewSIMUconfig={
+        'Nbiter_corr': ["2"  ,"1"  ,"1"  ,"1"   ,"3"  ,"2"  ,"1"  ,"2"  ,"4"  ,"3"],
+        'Nbmode_corr': ["500","800","500","1000","700","900","1000","900","700","900"]
     })
 print('time correction 2DM pw', time.time() - start_time)
+print("")
+print("")
+print("")
 
 # CORRECTION 2DM IN Perfect + EFC
 start_time = time.time()
@@ -77,9 +84,12 @@ phase, im = Main_EFC_THD.correctionLoop(
     Asterixroot + os.path.sep + 'Example_param_file.ini',
     NewDMconfig={'DM1_active': True},
     NewEstimationconfig={'estimation': 'Perfect'},
-    NewCorrectionconfig={
-        'circ_side': "Full",
-        'Nbiter_corr': '2  ,1  ,1  ,1   ,3  ,2  ,1  ,2  ,4  ,3',
-        'Nbmode_corr': '500,800,500,1000,700,900,1000,900,700,900'
+    NewCorrectionconfig={'circ_side': "Full"},
+    NewSIMUconfig={
+        'Nbiter_corr': ["2"  ,"1"  ,"1"  ,"1"   ,"3"  ,"2"  ,"1"  ,"2"  ,"4"  ,"3"],
+        'Nbmode_corr': ["500","800","500","1000","700","900","1000","900","700","900"]
     })
 print('total time correction 2DM perfect', time.time() - start_time)
+print("")
+print("")
+print("")
