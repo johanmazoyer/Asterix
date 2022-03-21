fa9d835cb0ed43e7b1a641230cfdef626a32ebeb
Johan Mazoyer <johan.mazoyer@obspm.fr>
2022-03-21 18:53:58 +0100

    correct and update parameter config check

--- Asterix/Example_Run_Asterix.py
+++ Asterix/Example_Run_Asterix.py
@@ -17,8 +17,8 @@ Main_THD.runthd2(Asterixroot + os.path.sep + 'Example_param_file.ini',
                      'correction_algorithm': "efc",
                  },
                  NewLoopconfig={
-                     'Nbiter_corr': ["5", "10"],
-                     "Nbmode_corr": ["320", "340"]
+                     'Nbiter_corr': [5, 10],
+                     "Nbmode_corr": [320, 340]
                  },
                  NewSIMUconfig={'Name_Experiment': "My_first_experiment"})
 print('time correction 1DM perfect estim efc', time.time() - start_time)
@@ -35,15 +35,14 @@ Main_THD.runthd2(Asterixroot + os.path.sep + 'Example_param_file.ini',
                      'correction_algorithm': "efc",
                  },
                  NewLoopconfig={
-                     'Nbiter_corr': ["5", "10"],
-                     "Nbmode_corr": ["320", "340"]
+                     'Nbiter_corr': [5, 10],
+                     "Nbmode_corr": [320, 340]
                  },
                  NewSIMUconfig={'Name_Experiment': "My_second_experiment"})
 print('time correction 1DM perfect estim efc', time.time() - start_time)
 print("")
 print("")
 print("")
-
 
 start_time = time.time()
 Main_THD.runthd2(Asterixroot + os.path.sep + 'Example_param_file.ini',
@@ -54,8 +53,8 @@ Main_THD.runthd2(Asterixroot + os.path.sep + 'Example_param_file.ini',
                      'correction_algorithm': "sm"
                  },
                  NewLoopconfig={
-                     'Nbiter_corr': ["20"],
-                     "Nbmode_corr": ["250"]
+                     'Nbiter_corr': [20],
+                     "Nbmode_corr": [250]
                  },
                  NewSIMUconfig={'Name_Experiment': "My_fourth_experiment"})
 print('time correction 2DM perfect estim sm', time.time() - start_time)
@@ -72,12 +71,9 @@ Main_THD.runthd2(Asterixroot + os.path.sep + 'Example_param_file.ini',
                      'correction_algorithm': "efc"
                  },
                  NewLoopconfig={
-                     'Nbiter_corr':
-                     ["5", "1", "1", "1", "3", "2", "1", "2", "4", "3"],
-                     "Nbmode_corr": [
-                         "500", "800", "500", "1000", "700", "900", "1000",
-                         "900", "700", "900"
-                     ]
+                     'Nbiter_corr': [5, 1, 1, 1, 3, 2, 1, 2, 4, 3],
+                     "Nbmode_corr":
+                     [500, 800, 500, 1000, 700, 900, 1000, 900, 700, 900]
                  },
                  NewSIMUconfig={'Name_Experiment': "My_fifth_experiment"})
 print('time correction 2DM pw efc', time.time() - start_time)
