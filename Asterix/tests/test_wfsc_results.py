74d6631c454515d0d22bf9b6b9c778d72c3f9131
Iva Laginja <iva.laginja@obspm.fr>
2022-09-23 17:07:17 +0200

    Define test cases for runthd2().

---
+++ Asterix/tests/test_wfsc_results.py
@@ -0,0 +1,13 @@
+def test_perfect_estimate_with_strokemin():
+    # Full DH and half DH
+    pass
+
+
+def test_pairwise_and_strokemin():
+    # Full DH and half DH
+    pass
+
+
+def test_pairwise_with_efc():
+    # Full DH and half DH
+    pass
