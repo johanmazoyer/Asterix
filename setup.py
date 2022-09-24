7e3a045e3fff7d3c44c6b98d38d94c063c58d721
Johan Mazoyer <johan.mazoyer@obspm.fr>
2022-09-24 10:53:00 +0200

    small modification to make the notebook work

--- setup.py
+++ setup.py
@@ -27,7 +27,9 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.8, 3.9, 3.10',
-        ],
+    ],
     keywords='Exoplanets imaging high-contrast coronagraphy',
-    install_requires=['astropy', 'configobj', 'jupyter', 'matplotlib', 'numpy', 'pytest', 'scikit-image', 'scipy']
-    )
+    install_requires=[
+        'astropy', 'configobj', 'jupyter', 'ipykernel', 'matplotlib', 'numpy', 'pytest', 'scikit-image',
+        'scipy'
+    ])
