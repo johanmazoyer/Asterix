99ffedf3558775bb1320d574ae27b1d940d37973
Iva Laginja <iva.laginja@obspm.fr>
2022-10-11 20:51:26 +0200

    Change outer-layer propagation in the function to use FFTs.

--- Asterix/optics/propagation_functions.py
+++ Asterix/optics/propagation_functions.py
@@ -624,9 +624,8 @@ def prop_fpm_regional_sampling(pup, fpm, nbres=np.array([0.1, 5, 50, 100]), samp
     sizebut_outer = dim_fpm / alpha * nbres[-1] / nbres_outer
     but_outer = 1 - butterworth_circle(dim_fpm, sizebut_outer, filter_order, xshift=-0.5, yshift=-0.5)
 
-    ef_pre_fpm_outer = mft(pup, real_dim_input=dim_pup, dim_output=dim_fpm, nbres=nbres_outer, inverse=True)
-    ef_pre_ls_outer = mft(ef_pre_fpm_outer * fpm * but_outer, real_dim_input=dim_fpm, dim_output=dim_fpm,
-                          nbres=nbres_outer, inverse=True)
+    ef_pre_fpm_outer = crop_or_pad_image(fft_choosecenter(pup, inverse=True), dim_fpm)
+    ef_pre_ls_outer = crop_or_pad_image(fft_choosecenter(ef_pre_fpm_outer * fpm * but_outer, inverse=True), dim_fpm)
 
     # Total E-field before the LS
     efield_before_ls += ef_pre_ls_outer
