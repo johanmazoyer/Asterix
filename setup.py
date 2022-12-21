cab067b8079e7f590ba336dee3a06a71c49bd7c7
Johan Mazoyer <johan.mazoyer@obspm.fr>
2022-12-21 13:39:48 +0100

    add yapf formater in the dev requirements

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
         "scipy",
     ],
     extras_require={
-        "dev": ["flake8", "jupyter", "pytest"],
+        "dev": ["flake8", "jupyter", "pytest", "yapf"],
         "docs": ["numpydoc", "sphinx-automodapi", "sphinx_rtd_theme"]
     })
