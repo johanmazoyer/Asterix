a167e3c9c74fdb33882ca593bc8dbf8f6132027f
Iva Laginja <iva.laginja@obspm.fr>
2024-09-03 17:38:53 +0200

    Loop up data generation

--- Asterix/example_run_asterix.py
+++ Asterix/example_run_asterix.py
@@ -1,5 +1,8 @@
+import itertools
 import os
 import time
+from astropy.io import fits
+import numpy as np
 from Asterix import main_THD, Asterix_root
 
 # please replace with your own parameter file
@@ -8,76 +11,31 @@ your_parameter_file_name = 'thd2_setups/Example_param_file_10jul24.ini'
 
 parameter_file_path = os.path.join(your_directory, your_parameter_file_name)
 
-start_time = time.time()
-main_THD.runthd2(parameter_file_path,
-                 NewDMconfig={'DM1_active': False},
-                 NewEstimationconfig={'estimation': 'pw'},
-                 NewCorrectionconfig={
-                     'DH_side': "Right",
-                     'correction_algorithm': "efc",
-                     'Nbmodes_OnTestbed': 330
-                 },
-                 NewLoopconfig={
-                     'Nbiter_corr': [5, 10],
-                     "Nbmode_corr": [330, 340]
-                 },
-                 NewSIMUconfig={'Name_Experiment': "My_first_experiment"},
-                 dir_save_all_planes=None)
+modesnb = 400  # 1587
 
-print('time correction 1DM perfect estim efc', time.time() - start_time)
-print("")
-print("")
-print("")
+dm3_actuators_array = fits.getdata('/Users/ilaginja/asterix_data/Labview/20240903_16-34-07_HLC_fb06a12d/Base_Matrix_DM3.fits')
+dm3_act_list = np.sum(dm3_actuators_array, axis=0)
+active_actuators = np.where(dm3_act_list != 0)
+actuator_pairs = itertools.combinations_with_replacement(active_actuators[0], r=2)
 
-start_time = time.time()
-main_THD.runthd2(parameter_file_path,
-                 NewDMconfig={'DM1_active': False},
-                 NewEstimationconfig={'estimation': 'perfect'},
-                 NewCorrectionconfig={
-                     'DH_side': "Right",
-                     'correction_algorithm': "efc",
-                     'Nbmodes_OnTestbed': 330
-                 },
-                 NewLoopconfig={
-                     'Nbiter_corr': [5, 10],
-                     "Nbmode_corr": [330, 340]
-                 },
-                 NewSIMUconfig={'Name_Experiment': "My_second_experiment"})
-print('time correction 1DM perfect estim efc', time.time() - start_time)
-print("")
-print("")
-print("")
+for pair in actuator_pairs:
+    print(f"Actuator pair: {pair}")
+    print(f'{pair[0]},{pair[1]}')
 
-start_time = time.time()
-main_THD.runthd2(parameter_file_path,
-                 NewDMconfig={'DM1_active': True},
-                 NewEstimationconfig={'estimation': 'perfect'},
-                 NewCorrectionconfig={
-                     'DH_side': "Full",
-                     'correction_algorithm': "sm",
-                 },
-                 NewLoopconfig={'Nbiter_corr': [20]},
-                 NewSIMUconfig={'Name_Experiment': "My_third_experiment"})
-print('time correction 2DM perfect estim sm', time.time() - start_time)
-print("")
-print("")
-print("")
-
-start_time = time.time()
-main_THD.runthd2(parameter_file_path,
-                 NewDMconfig={'DM1_active': True},
-                 NewEstimationconfig={'estimation': 'pw'},
-                 NewCorrectionconfig={
-                     'DH_side': "Full",
-                     'correction_algorithm': "efc",
-                     'Nbmodes_OnTestbed': 600
-                 },
-                 NewLoopconfig={
-                     'Nbiter_corr': [5, 1, 1, 1, 3, 2, 1, 2, 4, 3],
-                     'Nbmode_corr': [600, 800, 500, 1000, 700, 900, 1000, 900, 700, 900]
-                 },
-                 NewSIMUconfig={'Name_Experiment': "My_fourth_experiment"})
-print('time correction 2DM pw efc', time.time() - start_time)
-print("")
-print("")
-print("")
+    start_time = time.time()
+    main_THD.runthd2(parameter_file_path,
+                     NewDMconfig={'DM1_active': True},
+                     NewEstimationconfig={'estimation': 'pw',
+                                          'posprobes': (pair[0], pair[1])},
+                     NewCorrectionconfig={
+                         'DH_side': "Full",
+                         'correction_algorithm': "efc",
+                         'Nbmodes_OnTestbed': modesnb
+                     },
+                     NewLoopconfig={
+                         'Nbiter_corr': [50],
+                         'Nbmode_corr': [modesnb]
+                     },
+                     NewSIMUconfig={'Name_Experiment': f"HLC_783nm_{pair[0]}-{pair[1]}"})
+    print('time correction 2DM pw efc', time.time() - start_time)
+    print("")
