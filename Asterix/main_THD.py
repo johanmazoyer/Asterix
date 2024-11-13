7c92f6f51a6c40d09acd6095847d63dd77efdb0a
Iva Laginja <iva.laginja@obspm.fr>
2024-11-13 11:23:53 +0100

    Make optical elements of THD2 testbed class attributes

--- Asterix/main_THD.py
+++ Asterix/main_THD.py
@@ -45,17 +45,17 @@ class THD2(Testbed):
         # model_config['complex_precision'] = 'complex64'
 
         # Create all optical elements of the THD
-        entrance_pupil = Pupil(model_config,
-                               PupType=model_config["filename_instr_pup"],
-                               angle_rotation=model_config["entrance_pup_rotation"],
-                               Model_local_dir=model_local_dir,
-                               silence=silence)
-        dm1 = DeformableMirror(model_config, dm_config, Name_DM="DM1", Model_local_dir=model_local_dir, silence=silence)
-        dm3 = DeformableMirror(model_config, dm_config, Name_DM="DM3", Model_local_dir=model_local_dir, silence=silence)
-        corono = Coronagraph(model_config, corona_config, Model_local_dir=model_local_dir, silence=silence)
+        self.entrance_pupil = Pupil(model_config,
+                                    PupType=model_config["filename_instr_pup"],
+                                    angle_rotation=model_config["entrance_pup_rotation"],
+                                    Model_local_dir=model_local_dir,
+                                    silence=silence)
+        self.dm1 = DeformableMirror(model_config, dm_config, Name_DM="DM1", Model_local_dir=model_local_dir, silence=silence)
+        self.dm3 = DeformableMirror(model_config, dm_config, Name_DM="DM3", Model_local_dir=model_local_dir, silence=silence)
+        self.corono = Coronagraph(model_config, corona_config, Model_local_dir=model_local_dir, silence=silence)
 
         # Concatenate into the full testbed optical system
-        super().__init__([entrance_pupil, dm1, dm3, corono], ["entrancepupil", "DM1", "DM3", "corono"])
+        super().__init__([self.entrance_pupil, self.dm1, self.dm3, self.corono], ["entrancepupil", "DM1", "DM3", "corono"])
 
 
 def runthd2(parameter_file_path,
