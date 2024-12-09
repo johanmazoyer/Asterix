d3cbd1ec2671f9d1cb2a1998bd83ff0306b3b579
Iva Laginja <iva.laginja@obspm.fr>
2024-12-09 16:42:30 +0100

    Setup

--- Asterix/example_run_asterix.py
+++ Asterix/example_run_asterix.py
@@ -8,11 +8,11 @@ from Asterix import main_THD, Asterix_root
 
 # please replace with your own parameter file
 your_directory = Asterix_root
-your_parameter_file_name = 'thd2_setups/roman_637nm.ini'
+your_parameter_file_name = 'thd2_setups/Example_param_file_10jul24.ini'
 
 parameter_file_path = os.path.join(your_directory, your_parameter_file_name)
 
-modesnb = 300  # 1587
+modesnb = 320  # 1587
 
 ### SINGLE  RUN
 start_time = time.time()
@@ -29,7 +29,7 @@ main_THD.runthd2(parameter_file_path,
                      'Nbiter_corr': [50],
                      'Nbmode_corr': [modesnb]
                  },
-                 NewSIMUconfig={'Name_Experiment': f"HLC_783nm_actuators_0-11DH_{modesnb}modes"})
+                 NewSIMUconfig={'Name_Experiment': f"HLC_783nm_actuators_0-10DH_{modesnb}modes"})
                  # dir_save_all_planes='/Users/ilaginja/asterix_data/Results/all_planes')
 print('time correction 2DM pw efc', time.time() - start_time)
 print("")
